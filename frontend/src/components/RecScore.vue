692c0ab76e5f0950105266b451ab2d1b998f8c1a
DarleneQing <ophlia.cn@gmail.com>
2025-06-06 11:14:12 +0200

    reverse score blob order

--- frontend/src/components/RecScore.vue
+++ frontend/src/components/RecScore.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="blob-container">
-      <img src="@/assets/blobs/blob-1.svg" alt="Blob 1" class="blob" />
-      <img src="@/assets/blobs/blob-2.svg" alt="Blob 2" class="blob" />
-      <img src="@/assets/blobs/blob-3.svg" alt="Blob 3" class="blob" />
-      <img src="@/assets/blobs/blob-4.svg" alt="Blob 4" class="blob" />
       <img src="@/assets/blobs/blob-5.svg" alt="Blob 5" class="blob" />
+      <img src="@/assets/blobs/blob-4.svg" alt="Blob 4" class="blob" />
+      <img src="@/assets/blobs/blob-3.svg" alt="Blob 3" class="blob" />
+      <img src="@/assets/blobs/blob-2.svg" alt="Blob 2" class="blob" />
+      <img src="@/assets/blobs/blob-1.svg" alt="Blob 1" class="blob" />
     </div>
   </template>
   
